0bd70bf65513e6be9fa3a6b73d18c4d6826fb3f9
Gustavo del Pino <gpm314@gmail.com>
2016-12-12 23:24:51 -0500

    Started on server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 var express = require('express');
 
+var moment = require('moment');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = 8080;
+
 console.log('Test');
+
+app.listen( port, () => {
+    console.log("Server running at port " + port);
+});
+
